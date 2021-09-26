3071fd1661c39c63c0fd4a49a95e5845c571b543
Bruno Versiani Silva <bruno.versiani@gmail.com>
2021-09-26 09:59:44 -0300

    Criacao Modelo Categorias

--- ProcessoSeletivo/Controllers/HomeController.cs
+++ ProcessoSeletivo/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ProcessoSeletivo.Models.ViewModels;
 
 namespace ProcessoSeletivo.Controllers
 {
--- ProcessoSeletivo/Models/ViewModels/ErrorViewModel.cs
+++ ProcessoSeletivo/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace ProcessoSeletivo.Models
+namespace ProcessoSeletivo.Models.ViewModels
 {
     public class ErrorViewModel
     {
