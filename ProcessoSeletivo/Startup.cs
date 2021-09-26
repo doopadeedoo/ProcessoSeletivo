73819edfa7036b6958ce760171e0bdc29ae711f4
Bruno Versiani Silva <bruno.versiani@gmail.com>
2021-09-26 10:54:13 -0300

    Conexao com o banco SQL SERVER

--- ProcessoSeletivo/Startup.cs
+++ ProcessoSeletivo/Startup.cs
@@ -37,7 +37,8 @@ namespace ProcessoSeletivo
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ProcessoSeletivoContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ProcessoSeletivoContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("ProcessoSeletivoContext"), builder =>
+                        builder.MigrationsAssembly("ProcessoSeletivo")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
