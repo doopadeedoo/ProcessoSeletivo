9711f4fbfc6f2a5936ff25c610f3b7fdaf45a389
Bruno Versiani Silva <bruno.versiani@gmail.com>
2021-09-26 10:34:32 -0300

    CRUD Scaffold

---
+++ ProcessoSeletivo/Data/ProcessoSeletivoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProcessoSeletivo.Models
+{
+    public class ProcessoSeletivoContext : DbContext
+    {
+        public ProcessoSeletivoContext (DbContextOptions<ProcessoSeletivoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProcessoSeletivo.Models.Categoria> Categoria { get; set; }
+    }
+}
--- ProcessoSeletivo/Startup.cs
+++ ProcessoSeletivo/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProcessoSeletivo.Models;
 
 namespace ProcessoSeletivo
 {
@@ -33,6 +35,9 @@ namespace ProcessoSeletivo
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProcessoSeletivoContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProcessoSeletivoContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
